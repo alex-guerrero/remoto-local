f9c5028828cd907c59ba43cdbcb4808fe515588d
Alexander Guerrero <alez1881@gmail.com>
2017-04-02 19:47:49 -0500

    Se agregó nuevo método en la clase HelloWorld.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
+
 	public static void main(String[] args) {
 		System.out.println("Hello World :D");
+	}
+
+	public void nuevoMetodo() {
+		System.out.println("Soy un nuevo método");
 	}
 }

db62d882f7bb83f4fb1a15a440d7f65be2d7a780
Alexander Guerrero <alez1881@gmail.com>
2017-04-05 06:26:37 -0500

    Se agregó el método para sumar

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,10 @@ public class HelloWorld {
 		System.out.println("Hello World :D");
 	}
 
+	public int suma(int a, int b) {
+		return a+b;
+	}
+
 	public void nuevoMetodo() {
 		System.out.println("Soy un nuevo método");
 	}
